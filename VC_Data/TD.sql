0fd44e25435a840f48492bbe44436265f0816664
Jason Wang <jasonwong2019@outlook.com>
2024-03-26 21:46:55 +0800

    Complete the part of uploading predicted net demand to db.

--- VC_Data/TD.sql
+++
@@ -1,8 +0,0 @@
-SELECT [Date]
-                ,DATEPART(YEAR, Date) AS 'Year'
-                ,DATEPART(QUARTER, Date) AS 'Quarter'
-                ,[Period]
-                ,Demand+TCL+ISNULL(EnergyShortfall,0)+ISNULL(TransmissionLoss,0) AS 'Total_Demand'
-                ,DATEPART(WEEKDAY, Date) % 7 AS 'Day_Type'
-  FROM [NEMS].[dbo].[Real_Time_DPR]
-  WHERE [Date] >= '2023-07-01'
